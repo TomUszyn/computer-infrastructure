c2e879a88f21e3b671139210b63c3f836ad3081a
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-11-04 01:25:48 +0000

    Added solution to the task 7. Made changes in the weather.sh file.

--- weather.sh
+++ weather.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-wget -O data/weather/$(date +"%Y%m%d_%H%M%S").json https://prodapi.metweb.ie/observations/athenry/today
+#! /bin/bash
+
+wget -O data/weather/`date +"%Y%m%d_%H%M%S".json` https://prodapi.metweb.ie/observations/athenry/today
